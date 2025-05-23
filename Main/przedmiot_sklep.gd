f65b11217393a2e6826513a4dad9df6896bad56c
noone787 <witcherandmaster@op.pl>
2025-05-23 20:09:51 +0200

    magazyn i przesylka rzadu jest

---
+++ Main/przedmiot_sklep.gd
@@ -0,0 +1,12 @@
+extends TextureButton
+
+var Nazwa: String #Nazwa
+var stats: Resource
+
+func _ready() -> void:
+	pass
+	#To Działa Tylko Narazie Nie Ma Resourcesów Zrobionych --------------------
+	
+	#stats = load("res://Recourses/Products/%s.tres" % Nazwa) 
+	#stats.Textura = texture_normal
+	#stats.Tooltip = toltip_text
--- Main/shop_magazyn.gd
+++ Main/shop_magazyn.gd
@@ -1,1 +1,34 @@
 extends Control
+
+var Przedmiot #Obecnie Kliknięty Przedmiot
+#------------------------------------------------------
+var preload_path = preload("res://Main/przedmiot_sklep.tscn") # Preload Itemku
+@onready var h_box_container: HBoxContainer = $MarginContainer/VBoxContainer/HBoxContainer #Pierwszy HBox
+@onready var h_box_container_2: HBoxContainer = $MarginContainer/VBoxContainer/HBoxContainer2 #Drugi HBox
+@onready var h_box_container_3: HBoxContainer = $MarginContainer/VBoxContainer/HBoxContainer3 #Trzeci HBox
+#-------------------------------------------------------
+
+func _ready() -> void:
+	Rozstawienie()
+
+#Rozstawia Przedmioty Na Półkach
+func Rozstawienie():
+	for i in Global.Lista:
+		var item = preload_path.instantiate()
+		if h_box_container.get_child_count() < 7:
+			h_box_container.add_child(item)
+		elif h_box_container_2.get_child_count() < 7:
+			h_box_container_2.add_child(item)
+		elif h_box_container_3.get_child_count() < 7:
+			h_box_container_3.add_child(item)
+		
+		item.Nazwa = Global.Lista[i]
+		
+func _on_accept_pressed() -> void:
+	if Przedmiot != null:
+		Global.Przedmiot = Przedmiot
+		get_tree().change_scene_to_file("res://Main/Main.tscn")
+
+
+func _on_back_pressed() -> void:
+	get_tree().change_scene_to_file("res://Main/Main.tscn")
