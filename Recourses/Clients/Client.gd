da07361d0b1f49990ddfbee7d55505de078a2bf7
KamilZago <kamil.zagorny1802@gmail.com>
2025-05-23 19:49:11 +0200

    Clienci recoursy

--- Recourses/Clients/Client.gd
+++ Recourses/Clients/Client.gd
@@ -1,5 +1,24 @@
 extends Resource
 
-@export var Nazwa: String
+@export var Dialog: String
 
-@export var Textura: Texture2D
+@export var Items = {"jabłko" : 0.0,
+"chleb" : 0.0,
+"kiełbasa" : 0.0,
+"ziemniak" : 0.0,
+"mleko" : 0.0,
+"konserwa" : 0.0,
+"ocet" : 0.0,
+"papier toaletowy" : 0.0,
+"szare mydło" : 0.0,
+"pasta do zębów" : 0.0,
+"szczototka do kibla" : 0.0,
+"klucz hydrauliczny" : 0.0,
+"klej" : 0.0,
+"farba" : 0.0,
+"gwoździe" : 0.0,
+"gazeta" :  0.0,
+"książka marola karksa" : 0.0,
+"alkohol" : 0.0,
+"fajki" : 0.0,
+}
