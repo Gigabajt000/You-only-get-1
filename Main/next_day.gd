32afef95a02845be7fae5383484919e95d6d8f51
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-05-24 22:23:37 +0200

    wiecej butonuw

--- Main/next_day.gd
+++ Main/next_day.gd
@@ -9,3 +9,25 @@ func _on_pressed() -> void:
 	await Transition.on_transition_finished
 	get_tree().change_scene_to_file("res://Main/Main.tscn")
 	
+var tween: Tween
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+
+func reset_tween():
+	if tween:
+		tween.kill()
+	tween = create_tween()
+
+func _on_mouse_entered():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.05)
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.2)
+
+func _on_mouse_exited():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
