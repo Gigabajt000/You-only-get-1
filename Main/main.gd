151950524a3d719b17cacdab75b353fc2d443ee4
F1ejm <karolosrodka@gmail.com>
2025-05-24 12:32:08 +0200

    jhdfsg

---
+++ Main/main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var dzien : int 
+
+func _ready() -> void:
+	dzien =Global.Dzien
+
+func _process(delta: float) -> void:
+	if dzien != Global.Dzien:
+		dzien = Global.Dzien
+		get_tree().change_scene_to_file("res://Main/week_end.tscn")
--- Main/week_end.gd
+++ Main/week_end.gd
@@ -1,5 +1,5 @@
 extends Control
 
 func _ready() -> void:
-	$AnimationPlayer/Node2D.position.x = Global.Dzien * 140
+	$AnimationPlayer/Node2D.position.x = (Global.Dzien - 1) * 140
 	$AnimationPlayer.play("przesowanie")
