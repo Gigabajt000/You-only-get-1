9801d23ec81991e295e2737354ccb0c6e7437f5e
noone787 <witcherandmaster@op.pl>
2025-05-24 23:24:44 +0200

    Reapply "Merge branch 'main' of github.com:Gigabajt000/You-only-get-1"
    This reverts commit ae69b8bf810e935ba82bc443dc5c79cdee735f96.

--- Main/accept_button.gd
+++ Main/accept_button.gd
@@ -3,6 +3,9 @@ extends TextureButton
 func _ready() -> void:
 	$"../Magazyn_Button".visible = true
 	visible = true
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
 #Podanie Przedmiotu Klientowi
 func _on_pressed() -> void:
 	#Animacja Podania Przedmiotu
@@ -27,3 +30,21 @@ func _on_pressed() -> void:
 		Global.podany = true
 func _on_timer_timeout() -> void:
 	Global.Dzien +=1
+
+var tween: Tween
+
+func reset_tween():
+	if tween:
+		tween.kill()
+	tween = create_tween()
+
+func _on_mouse_entered():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.05)
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.2)
+
+func _on_mouse_exited():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
--- Main/magazyn_button.gd
+++ Main/magazyn_button.gd
@@ -3,3 +3,26 @@ extends TextureButton
 
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://Main/shop_magazyn.tscn")
+
+var tween: Tween
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+
+func reset_tween():
+	if tween:
+		tween.kill()
+	tween = create_tween()
+
+func _on_mouse_entered():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.05)
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.2)
+
+func _on_mouse_exited():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
