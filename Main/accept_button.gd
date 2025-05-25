0e7968dbe67e90d53c61b35f4a9f1e9395d7c100
noone787 <witcherandmaster@op.pl>
2025-05-25 11:26:25 +0200

    erergerg

--- Main/accept_button.gd
+++ Main/accept_button.gd
@@ -3,6 +3,11 @@ extends TextureButton
 func _ready() -> void:
 	$"../Magazyn_Button".visible = true
 	visible = true
+	
+func _process(delta: float) -> void:
+	if $"../Control".texture_normal == null:
+		texture_normal = preload("res://Maro_ZdjęcieOG.png")
+
 #Podanie Przedmiotu Klientowi
 func _on_pressed() -> void:
 	#Animacja Podania Przedmiotu
