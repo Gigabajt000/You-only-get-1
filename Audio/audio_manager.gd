d04e4aaa76aa81543045567bbccd39c769ead2e3
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-05-25 21:17:23 +0200

    muzyka

--- Audio/audio_manager.gd
+++ Audio/audio_manager.gd
@@ -1,10 +1,15 @@
 extends AudioStreamPlayer
 
+@onready var music: AudioStreamPlayer = $SovietMarchRoyaltyFreeRussianOrchestralMusic
+
 @onready var button_hover: AudioStreamPlayer = $ButtonHover
 
 @onready var buy_1: AudioStreamPlayer = $Buy/Buy1
 @onready var buy_2: AudioStreamPlayer = $Buy/Buy2
 @onready var buy_3: AudioStreamPlayer = $Buy/Buy3
+
+@onready var menu_button_down: AudioStreamPlayer = $MenuButtonDown
+@onready var menu_button_up: AudioStreamPlayer = $MenuButtonUp
 
 func play_random_accept_sound():
 	var x = randi_range(0,2)
