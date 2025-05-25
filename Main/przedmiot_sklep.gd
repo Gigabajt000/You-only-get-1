0e2e8f50eb57d5bc5e7ad54c3d1cadc37ed88f20
noone787 <witcherandmaster@op.pl>
2025-05-25 15:30:56 +0200

    zdgfzxdcbzdfvgb

--- Main/przedmiot_sklep.gd
+++ Main/przedmiot_sklep.gd
@@ -4,6 +4,8 @@ var Nazwa: String #Nazwa
 var stats: Resource
 
 func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
 	#To Działa Tylko Narazie Nie Ma Resourcesów Zrobionych --------------------
 	stats = load("res://Recourses/Products/%s.tres" % Nazwa) 
 	texture_normal = stats.Textura 
@@ -17,3 +19,21 @@ func _on_pressed() -> void:
 	Global.Przedmiot = Nazwa
 	Global.Lista.erase(Nazwa)
 	get_tree().change_scene_to_file("res://Main/Main.tscn")
+
+var tween: Tween
+
+func reset_tween():
+	if tween:
+		tween.kill()
+	tween = create_tween()
+
+func _on_mouse_entered():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.05)
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.2)
+
+func _on_mouse_exited():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
