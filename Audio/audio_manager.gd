87ac074e77d6e884765bd1b0842ea7f75fe14574
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-05-25 19:21:18 +0200

    tup tup

---
+++ Audio/audio_manager.gd
@@ -0,0 +1,9 @@
+extends AudioStreamPlayer
+
+@onready var gadanie_1: AudioStreamPlayer = $Gadanie/Gadanie1
+@onready var gadanie_2: AudioStreamPlayer = $Gadanie/Gadanie2
+@onready var gadanie_3: AudioStreamPlayer = $Gadanie/Gadanie3
+
+@onready var buy_1: AudioStreamPlayer = $Buy/Buy1
+@onready var buy_2: AudioStreamPlayer = $Buy/Buy2
+@onready var buy_3: AudioStreamPlayer = $Buy/Buy3
