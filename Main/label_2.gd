c6800a7b293c17fa425b3c907c6da80a0de3402c
F1ejm <karolosrodka@gmail.com>
2025-05-25 18:21:04 +0200

    tween

--- Main/label_2.gd
+++ Main/label_2.gd
@@ -1,9 +1,15 @@
 extends Label
 
+
+
 func _ready() -> void:
 	visible = false
+	
+	
 
 func _process(delta: float) -> void:
+	position.x = 772.0
+	position.y = 50
 	text = str(Global.punkty_po_podaniu * 10 * Global.Dodatkowy_Hajs)
 	if Global.punkty_po_podaniu * 10 < 0:
 		label_settings.font_color = Color(1,0,0)
@@ -11,10 +17,14 @@ func _process(delta: float) -> void:
 		label_settings.font_color = Color(0,1,0)
 		
 	if Global.Start_Timer == true:
+		var tween = create_tween()
+		modulate.a = 1
 		Global.Start_Timer = false
 		$Timer.start()
 		visible = true
-		
+		tween.set_parallel(true) 
+		tween.tween_property($".","global_position",Vector2(772,10),1)
+		tween.tween_property($".","modulate:a",0,1)
 
 func _on_timer_timeout() -> void:
 	visible = false
