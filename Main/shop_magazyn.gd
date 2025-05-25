844a58534164d1f16bc87356f644014b5b015f67
KamilZago <kamil.zagorny1802@gmail.com>
2025-05-25 11:14:38 +0200

    shop spacing adjustment

--- Main/shop_magazyn.gd
+++ Main/shop_magazyn.gd
@@ -2,9 +2,9 @@ extends Control
 
 #------------------------------------------------------
 var preload_path = preload("res://Main/przedmiot_sklep.tscn") # Preload Itemku
-@onready var h_box_container: HBoxContainer = $MarginContainer/VBoxContainer/HBoxContainer #Pierwszy HBox
-@onready var h_box_container_2: HBoxContainer = $MarginContainer/VBoxContainer/HBoxContainer2 #Drugi HBox
-@onready var h_box_container_3: HBoxContainer = $MarginContainer/VBoxContainer/HBoxContainer3 #Trzeci HBox
+@onready var h_box_container: HBoxContainer = $Node2D/MarginContainer/VBoxContainer/HBoxContainer #Pierwszy HBox
+@onready var h_box_container_2: HBoxContainer = $Node2D/MarginContainer/VBoxContainer/HBoxContainer2 #Drugi HBox
+@onready var h_box_container_3: HBoxContainer = $Node2D/MarginContainer/VBoxContainer/HBoxContainer3 #Trzeci HBox
 #-------------------------------------------------------
 
 func _ready() -> void:
@@ -17,10 +17,13 @@ func Rozstawienie():
 		item.Nazwa = i
 		if h_box_container.get_child_count() < 7:
 			h_box_container.add_child(item)
+			$Node2D.position.y=0
 		elif h_box_container_2.get_child_count() < 7:
 			h_box_container_2.add_child(item)
+			$Node2D.position.y=25
 		elif h_box_container_3.get_child_count() < 7:
 			h_box_container_3.add_child(item)
+			$Node2D.position.y=80
 
 
 func _on_back_pressed() -> void:
