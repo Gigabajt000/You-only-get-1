03c51787fee82bab9d647c21176e18f99ad9e4d6
Gigabajt000 <nikitadyrcz8@gmail.com>
2025-05-25 15:22:16 +0200

    asdad

--- Main/control.gd
+++ Main/control.gd
@@ -19,3 +19,25 @@ func _on_pressed() -> void:
 	Global.Lista.append(Global.Przedmiot)
 	Global.Przedmiot = ""
 	
+var tween: Tween
+
+func _ready() -> void:
+	mouse_entered.connect(_on_mouse_entered)
+	mouse_exited.connect(_on_mouse_exited)
+
+
+func reset_tween():
+	if tween:
+		tween.kill()
+	tween = create_tween()
+
+func _on_mouse_entered():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.2, 1.2), 0.05)
+	tween.tween_property(self, "scale", Vector2(1.1, 1.1), 0.2)
+
+func _on_mouse_exited():
+	reset_tween()
+	tween.set_trans(Tween.TRANS_CUBIC)
+	tween.tween_property(self, "scale", Vector2(1.0, 1.0), 0.1)
