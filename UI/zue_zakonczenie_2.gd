d3b37157e6674799006f516e1cf67ab67f64d14e
noone787 <witcherandmaster@op.pl>
2025-05-25 21:01:26 +0200

    adrfgzerg

--- UI/zue_zakonczenie_2.gd
+++ UI/zue_zakonczenie_2.gd
@@ -6,4 +6,4 @@ func _ready():
 func _on_timer_timeout() -> void:
 	Transition.transition()
 	await Transition.on_transition_finished
-	get_tree().change_scene_to_file("res://UI/zue_zakonczenie_2.tscn")
+	get_tree().change_scene_to_file("res://UI/zue_zakonczenie_3.tscn")
